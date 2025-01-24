25542557e6b37b497e87b883453774445117115f
Aasiya Shaikh <aasiya.shaikh@dgv.in>
2025-01-24 12:28:33 +0530

    Navigation dependency, dashboard navgraph, profileFragment, libraryFragment, searchFragment, premiumFragment

--- app/src/main/java/com/example/sunnzaraa/MainActivity.kt
+++ app/src/main/java/com/example/sunnzaraa/MainActivity.kt
@@ -2,10 +2,48 @@ package com.example.sunnzaraa
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.NavController
+import androidx.navigation.NavHost
+import androidx.navigation.fragment.NavHostFragment
+import com.example.sunnzaraa.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+//
+    private lateinit var navController: NavController
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding =ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        val navHostFragment = supportFragmentManager.findFragmentById(R.id.container) as NavHostFragment
+        navController = navHostFragment.navController
+
+        binding.homeToolbar.iconProfile.setOnClickListener {
+            navController.navigate(R.id.profileScreenFragment)
+        }
+        setUpHomeBottomBar()
+    }
+
+    private fun setUpHomeBottomBar(){
+        binding.homeBottomBar.apply {
+            iconHome.setOnClickListener {
+                navController.navigate(R.id.homeScreenFragment)
+            }
+
+            iconLibrary.setOnClickListener {
+                navController.navigate(R.id.libraryScreenFragment)
+            }
+
+            iconSearch.setOnClickListener {
+                navController.navigate(R.id.searchScreenFragment)
+            }
+
+            iconPremium.setOnClickListener {
+                navController.navigate(R.id.premiumScreenFragment)
+            }
+        }
     }
 }
