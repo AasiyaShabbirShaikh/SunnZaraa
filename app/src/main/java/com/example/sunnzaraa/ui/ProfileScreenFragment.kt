db34acca78e9c58a29e60c7c703c9fd3b48ae2cf
Aasiya Shaikh <aasiya.shaikh@dgv.in>
2025-01-27 18:34:06 +0530

    build.gradle recovered, accountSettingsScreenFragment -> settingsScreenFragment, EditScreenFragment, profileScreen Fragment

--- app/src/main/java/com/example/sunnzaraa/ui/ProfileScreenFragment.kt
+++ app/src/main/java/com/example/sunnzaraa/ui/ProfileScreenFragment.kt
@@ -6,14 +6,19 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import com.example.sunnzaraa.R
+import com.example.sunnzaraa.databinding.FragmentPremiumScreenBinding
+import com.example.sunnzaraa.databinding.FragmentProfileScreenBinding
 
 class ProfileScreenFragment : Fragment() {
+
+    private lateinit var binding: FragmentProfileScreenBinding
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        return inflater.inflate(R.layout.fragment_profile_screen, container, false)
+        binding= FragmentProfileScreenBinding.inflate(layoutInflater, container, false)
+        return binding.root
     }
 
 }
--- app/src/main/java/com/example/sunnzaraa/ui/SettingsScreenFragment.kt
+++ app/src/main/java/com/example/sunnzaraa/ui/SettingsScreenFragment.kt
@@ -7,13 +7,13 @@ import android.view.View
 import android.view.ViewGroup
 import com.example.sunnzaraa.R
 
-class AccountSettingsScreenFragment : Fragment() {
+class SettingsScreenFragment : Fragment() {
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        return inflater.inflate(R.layout.fragment_account_settings_screen, container, false)
+        return inflater.inflate(R.layout.fragment_settings_screen, container, false)
     }
 
 }
