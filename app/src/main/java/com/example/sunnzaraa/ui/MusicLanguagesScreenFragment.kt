6acb6169016b1bcfac04370a63542ce167351db8
Aasiya Shaikh <aasiya.shaikh@dgv.in>
2025-01-28 17:55:12 +0530

    musicLanguagesFragment completed, albumfragment completed

--- app/src/main/java/com/example/sunnzaraa/ui/MusicLanguagesScreenFragment.kt
+++ app/src/main/java/com/example/sunnzaraa/ui/MusicLanguagesScreenFragment.kt
@@ -5,15 +5,49 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.navigation.fragment.findNavController
 import com.example.sunnzaraa.R
+import com.example.sunnzaraa.databinding.FragmentMusicLanguagesScreenBinding
 
 class MusicLanguagesScreenFragment : Fragment() {
+
+    private lateinit var binding: FragmentMusicLanguagesScreenBinding
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        return inflater.inflate(R.layout.fragment_music_languages_screen, container, false)
+        binding = FragmentMusicLanguagesScreenBinding.inflate(layoutInflater, container, false)
+
+        musicLanguageOptionClick()
+        return binding.root
+    }
+
+    private fun musicLanguageOptionClick(){
+        binding.englishCardview.setOnClickListener {
+            findNavController().navigate(R.id.albumScreenFragment)
+        }
+
+        binding.hindiCardview.setOnClickListener {
+            findNavController().navigate(R.id.albumScreenFragment)
+        }
+
+        binding.marathiCardview.setOnClickListener {
+            findNavController().navigate(R.id.albumScreenFragment)
+        }
+
+        binding.punjabiCardview.setOnClickListener {
+            findNavController().navigate(R.id.albumScreenFragment)
+        }
+
+        binding.gujaratiCardview.setOnClickListener {
+            findNavController().navigate(R.id.albumScreenFragment)
+        }
+
+        binding.southCardview.setOnClickListener {
+            findNavController().navigate(R.id.albumScreenFragment)
+        }
+
     }
 
 }
